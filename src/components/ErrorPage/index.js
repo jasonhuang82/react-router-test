b26931fa368c9055fd30a0839a1947e9d771195d
Unknown <jasonhuang702@gmail.com>
2018-07-01 11:22:51 +0800

    init file

---
+++ src/components/ErrorPage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './css.scss';
+
+const ErrorPage = props => (
+  <div>
+    Error 404 !! this page is not exist~
+  </div>
+);
+
+export default ErrorPage;
